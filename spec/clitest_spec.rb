e83b793cf298803e663edba9c9ad8a58d5ebf217
yasuo kominami <ykominami@gmail.com>
2021-03-14 08:54:33 +0900

    add spec

--- spec/clitest_spec.rb
+++ spec/clitest_spec.rb
@@ -1,6 +1,18 @@
 # frozen_string_literal: true
+require 'spec_helper'
+require 'pathname'
 
 RSpec.describe Clitest do
+  let(:test_dir_pn){ Pathname.new(__FILE__).parent + 'test' }
+  let(:test_dir){ test_dir_pn.to_s }
+  let(:test_cmd_1){ 'test_cmd_1'}
+  let(:test_cmd_2){ 'test_cmd_2'}
+  let(:target_dir){ (test_dir_pn + 'cmd1').to_s }
+  let(:result){'result.txt'}
+  let(:optionx){nil}
+  let(:param_name){nil}
+
+  let(:cmdline0) { Clitest::Cmdline.new(test_dir, test_cmd_1, test_cmd_2) }
   it "has a version number" do
     expect(Clitest::VERSION).not_to be nil
   end
@@ -8,11 +20,25 @@ RSpec.describe Clitest do
   context "create instance of class" do
     context "Cmdline" do
       before(:each)  do
-        @cmdline = Cmdline.new
+        @cmdline = Clitest::Cmdline.new(test_dir)
       end
       it "create instance" do
         expect(@cmdline).not_to eq(nil)
       end
+    end
+  end
+
+  context "make argument" do
+    before(:each)  do
+      @ret_cmd_1 = cmdline0.make_cmdline_1(target_dir, result, optionx, param_name)
+      @ret_cmd_2 = cmdline0.make_cmdline_1(target_dir, result, optionx, param_name)
+    end
+
+    it "of test_cmd_1" do
+      expect(@ret_cmd_1).not_to eq(nil)
+    end
+    it "of test_cmd_2" do
+      expect(@ret_cmd_2).not_to eq(nil)
     end
   end
 end
