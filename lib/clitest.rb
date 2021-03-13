4927f1d9c54ebc0e561d30cbd7d25f0c9972a0cf
yasuo kominami <ykominami@gmail.com>
2021-03-14 08:58:34 +0900

    adujst cmd line arguments

--- lib/clitest.rb
+++ lib/clitest.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "clitest/version"
+require_relative "clitest/cmdline"
 
 module Clitest
   class Error < StandardError; end
--- lib/clitest/cmdline.rb
+++ lib/clitest/cmdline.rb
@@ -4,31 +4,29 @@ module Clitest
   require "pathname"
 
   class Cmdline
-    def initialize(test_data_dir, target_cmd_1, target_cmd_2)
-      bin_pn = "#{Pathname.new(__FILE__).parent.parent}bin"
-      @test_cmd_path_pn = "#{bin_pn}cmd.sh"
-      @test_data_dir_pn = Pathname.new(test_data_dir)
-      @target_cmd = []
-      @target_cmd << target_cmd_1
-      @target_cmd << target_cmd_2
+    def initialize(test_dir, *target_cmds)
+      bin_pn = Pathname.new(__FILE__).parent.parent.parent + 'bin'
+      @test_cmd_path_pn = bin_pn + 'cmd.sh'
+      @test_dir_pn = Pathname.new(test_dir)
+      @target_cmd = target_cmds
     end
 
     def make_cmdline_base(target_cmd, target_dir, result, optionx, *param_names)
       params = param_names.join(" ")
 
-      "#{@test_cmd_path_pn} #{@test_data_dir_pn} #{result} #{target_cmd} #{target_dir} #{optionx} #{params}"
+      "#{@test_cmd_path_pn} #{@test_dir_pn} #{result} #{target_cmd} #{target_dir} #{optionx} #{params}"
     end
 
     def make_cmdline(cmd_index, target_dir, result, optionx, *param_names)
       make_cmdline_base(@target_cmd[cmd_index], target_dir, result, optionx, *param_names)
     end
 
-    def make_cmdline_1(target_dir, result, optionx, *_param_name)
-      make_cmdline(0, @target_cmd_2, target_dir, result, optionx, *param_names)
+    def make_cmdline_1(target_dir, result, optionx, *param_name)
+      make_cmdline(0, target_dir, result, optionx, param_name)
     end
 
-    def make_cmdline_2(target_dir, result, optionx, *_param_name)
-      make_cmdline(1, target_dir, result, optionx, *param_names)
+    def make_cmdline_2(target_dir, result, optionx, *param_name)
+      make_cmdline(1, target_dir, result, optionx, param_name)
     end
   end
 end
