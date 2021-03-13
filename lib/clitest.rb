7adeb182f2bfff8f8a1037ffaaeb99e91cf0027a
yasuo kominami <ykominami@gmail.com>
2021-03-13 18:44:59 +0900

    initial commit

---
+++ lib/clitest.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "clitest/version"
+
+module Clitest
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/clitest/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Clitest
+  VERSION = "0.1.0"
+end
