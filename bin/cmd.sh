35c63ee5efa1f10f4f84773ca9c248d6fc21a30f
yasuo kominami <ykominami@gmail.com>
2021-03-14 08:54:53 +0900

    add bin/cmd.sh

--- bin/cmd.sh
+++ bin/cmd.sh
@@ -1,29 +1,35 @@
 #!/bin/bash
 function do_test()
 {
-   TECSGEN=$1
+   target_cmd=$1
    shift
    pushd $1
    shift
-   echo "##### $TECSGEN $*  #####"
-   $TECSGEN $*
+   echo "##### ${target_cmd} $*  #####"
+   $target_cmd $*
    exit_code=$?
    popd
 
    return $exit_code
 }
 
+# test_dir result_file target_cmd target_dir args
+# bash /home/ykominami/repo/clitest/bin/cmd.sh 
+# /home/ykominami/repo/clitest/spec/test cmd1 ls  result.txt
 echo $*
-test_data_dir=$1
+test_dir=$1
 shift
 result_file=$1
 shift
-# TECSGEN=$1
-# shift
-# pushd $1
-# shift
-# echo "##### $TECSGEN $*  #####"
-# $TECSGEN $* | tee $result_file
-# popd
-do_test $* | tee $result_file
+target_dir=$2
+if [ ! -e $target_dir ];then
+   mkdir ${target_dir}
+fi
+result_path="${test_dir}/${target_dir}/${result_file}"
+# echo "result_path=${result_path}"
+if [ ! -f ${result_path} ]; then
+   touch ${result_path}
+fi
+
+do_test $* | tee $result_path
 exit $?
